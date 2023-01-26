9a8f51d5b89b261fce2af93cb4790a0a3afd0dcb
Mark Venema <markavenema@gmail.com>
2023-01-26 20:57:58 +0900

    Split Screen Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { SplitScreen } from "./SplitScreen";
+
+const LeftHandComponent = ({ name }) => {
+    return <h1 style={{backgroundColor: 'green'}}>{name}!</h1>
+}
+
+const RightHandComponent = ({ message }) => {
+    return <p style={{backgroundColor: 'red'}}>{message}!</p>
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    return (
+        <SplitScreen 
+            leftWeight={1}
+            rightWeight={3}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <LeftHandComponent name="Mark" />
+            <RightHandComponent message="Hello" />
+        </SplitScreen>
+    );
 }
 
 export default App;
