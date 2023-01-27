cc826f7f570b0b2f0313efdf1a9b1217b9082c00
Mark Venema <markavenema@gmail.com>
2023-01-28 07:46:43 +0900

    Lists

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
+import { RegularList } from './RegularList';
+import { NumberedList } from './NumberedList';
+import { SmallPersonListItem } from './people/SmallPersonListItem';
+import { LargePersonListItem } from './people/LargePersonListItem';
+import { SmallProductListItem } from './products/SmallProductListItem';
+import { LargeProductListItem } from './products/LargeProductListItem';
+const people = [{
+    name: 'John Doe',
+    age: 54,
+    hairColor: 'brown',
+    hobbies: ['swimming', 'bicycling', 'video games'],
+},{
+    name: 'Brenda Smith',
+    age: 33,
+    hairColor: 'black',
+    hobbies: ['golf', 'mathematics'],
+},
+{
+    name: 'Jane Garcia',
+    age: 27,
+    hairColor: 'blonde',
+    hobbies: ['biology', 'medicine', 'gymnastics'],
+}]
+
+const products = [{
+    name: 'Flat-Screen TV',
+    price: '$300',
+    description: 'Huge LCD screen, a great deal',
+    rating: 4.5,
+},{
+    name: 'Basketball',
+    price: '$10',
+    description: 'Just like the pros use',
+    rating: 3.8,
+},{
+    name: 'Running Shoes',
+    price: '$120',
+    description: 'State-of-the-art technology for optimum running',
+    rating: 4.2,
+}]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <RegularList 
+        items={people}
+        resourceName="person"
+        itemComponent={SmallPersonListItem} />
+    <RegularList 
+        items={people}
+        resourceName="person"
+        itemComponent={LargePersonListItem}
+    />
+    <RegularList 
+        items={products}
+        resourceName="product"
+        itemComponent={SmallProductListItem}
+    />
+    <RegularList 
+        items={products}
+        resourceName="product"
+        itemComponent={LargeProductListItem}
+    />
+    <NumberedList 
+        items={people}
+        resourceName="person"
+        itemComponent={SmallPersonListItem} />
+    </>
+    
   );
 }
 
