27e0a8f60348152165a1dfc7ceefbc1d2ca722a1
Mark Venema <markavenema@gmail.com>
2023-02-01 07:28:03 +0900

    Controlled vs Uncontrolled

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
+import { UncontrolledForm } from "./UncontrolledForm";
+import { ControlledForm } from "./ControlledForm";
+import { UncontrolledModal } from './UncontrolledModal';
+import { ControlledModal } from "./ControlledModal";
 function App() {
+    const [shouldShowModal, setShouldShowModal] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+        <ControlledModal 
+            shouldShow={shouldShowModal}
+            onRequestClose={() => setShouldShowModal(false)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+            <h1>Hello</h1>
+        </ControlledModal>
+        <button onClick={() => setShouldShowModal(!shouldShowModal)}>
+            {shouldShowModal ? 'Hide Modal' : 'Show Modal'}
+        </button>
+    </>
   );
 }
 
