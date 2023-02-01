a34bf7a68d61584654d9d7e0157fbcaecb1397bc
Mark Venema <markavenema@gmail.com>
2023-02-02 07:55:22 +0900

    HOC printing props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { printProps } from "./printProps";
+import { UserInfo } from "./UserInfo";
+
+const UserInfoWrapped = printProps(UserInfo);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <UserInfoWrapped a={1} b="Hello" c={{name: 'Joe'}} />
   );
 }
 
