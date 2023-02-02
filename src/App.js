da727becf6adae5ca82d19b1eba32f59aa80b40c
Mark Venema <markavenema@gmail.com>
2023-02-02 19:11:40 +0900

    HOC loading props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { printProps } from "./printProps";
 import { UserInfo } from "./UserInfo";
+import { withUser } from "./withUser";
 
-const UserInfoWrapped = printProps(UserInfo);
+const UserInfoWithLoader = withUser(UserInfo, '234');
 
 function App() {
   return (
-        <UserInfoWrapped a={1} b="Hello" c={{name: 'Joe'}} />
+        <UserInfoWithLoader />
   );
 }
 
