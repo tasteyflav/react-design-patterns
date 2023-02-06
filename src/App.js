cd0aa3f9b503a9589ddb8688f48a853f733b72ac
Mark Venema <markavenema@gmail.com>
2023-02-06 20:00:33 +0900

    11-custom-hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { UserInfo } from "./UserInfo";
+import { ProductInfo } from "./ProductInfo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <UserInfo userId="123"/>
+    <ProductInfo productId="1234" />
+    </>
+    
   );
 }
 
---
+++ src/useDataSource.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export const useDataSource = getResourceFunc => {
+    const [resource, setResource] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            const result = await getResourceFunc();
+            setResource(result);
+        })();
+    }, [getResourceFunc])
+
+    return resource;
+}
